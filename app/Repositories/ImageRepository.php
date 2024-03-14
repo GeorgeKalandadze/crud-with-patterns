0d7c58201363b053306c156860f6ec78773faae8
George Kalandadze <kalandadzeg777@gmail.com>
2024-03-14 17:05:49 +0400

    feat/make contract and repository for image

---
+++ app/Repositories/ImageRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Image;
+
+class ImageRepository implements Contracts\ImageRepositoryContract
+{
+
+    public function getAll()
+    {
+        return Image::all();
+    }
+
+    public function create(array $data)
+    {
+        return Image::create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $image = Image::findOrFail($id);
+        $image->update($data);
+        return $image;
+    }
+
+    public function delete(int $id)
+    {
+        return Image::destroy($id);
+    }
+
+    public function getById(int $id)
+    {
+        return Image::findOrFail($id);
+    }
+}
